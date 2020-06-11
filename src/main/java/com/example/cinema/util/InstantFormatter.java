d902e39fafecd29d96b722dc89b8e693c7ee3529
Ilya Kisel <ikisel@touchlane.com>
2020-06-11 12:34:00 +0300

    date time format

--- src/main/java/com/example/cinema/util/InstantFormatter.java
+++ src/main/java/com/example/cinema/util/InstantFormatter.java
@@ -1,5 +1,7 @@
 package com.example.cinema.util;
 
+import lombok.extern.slf4j.Slf4j;
+
 import java.time.Instant;
 import java.time.ZoneId;
 import java.time.ZoneOffset;
@@ -7,16 +9,18 @@ import java.time.format.DateTimeFormatter;
 import java.time.format.FormatStyle;
 import java.util.Locale;
 
+@Slf4j
 public class InstantFormatter {
+    public static final ZoneOffset MINSK_OFFSET = ZoneOffset.ofHours(3);
     private static final DateTimeFormatter formatter = DateTimeFormatter
             .ofLocalizedDateTime(FormatStyle.LONG)
             .withLocale(Locale.UK)
-            .withZone(ZoneId.systemDefault());
-
-    public static final ZoneOffset MINSK_OFFSET = ZoneOffset.ofHours(3);
+            .withZone(ZoneId.ofOffset("UTC", MINSK_OFFSET));
 
     public static String format(Instant instant) {
         String formatted = formatter.format(instant);
-        return formatted.substring(0, formatted.length() - 4);
+        log.info("Formatted date UTC+03:00");
+        log.info(formatted);
+        return formatted.substring(0, formatted.length() - 9);
     }
 }
